f28e9ca9f2eb40867a2124e8516aba1fab151e14
sibbe23 <sibbe23@gmail.com>
2024-05-23 21:04:01 +0530

    Final

--- views/public/service-worker.js
+++ views/public/service-worker.js
@@ -1,20 +1,20 @@
--self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('v1').then(cache => {
-        return cache.addAll([
-          '/views/public/html/loginpage.html',
-          '/views/public/html/login.js',
-          '/views/public/js/script.js',
-        ]);
-      })
-    );
-  });
+// -self.addEventListener('install', event => {
+//     event.waitUntil(
+//       caches.open('v1').then(cache => {
+//         return cache.addAll([
+//           '/views/public/html/loginpage.html',
+//           '/views/public/html/login.js',
+//           '/views/public/js/script.js',
+//         ]);
+//       })
+//     );
+//   });
   
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
+//   self.addEventListener('fetch', event => {
+//     event.respondWith(
+//       caches.match(event.request).then(response => {
+//         return response || fetch(event.request);
+//       })
+//     );
+//   });
   
