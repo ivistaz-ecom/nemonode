5ca4ef1e480f9030f6e02d05cdddad8c9562f61d
sibbe23 <sibbe23@gmail.com>
2024-05-23 20:40:08 +0530

    new features 3

--- views/public/js/script.js
+++ views/public/js/script.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/views/public/service-worker.js').then(registration => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        console.log('Servers Active ', registration.scope);
       }, error => {
-        console.log('ServiceWorker registration failed: ', error);
+        console.log('Servers not Active: ', error);
       });
     });
   }
