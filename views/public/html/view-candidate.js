f493b2ad06ae05bfb5ce2ee1d99615ce18353e59
sibbe23 <sibbe23@gmail.com>
2024-06-07 11:31:44 +0530

    storage new 4

--- views/public/html/view-candidate.js
+++ views/public/html/view-candidate.js
@@ -231,7 +231,6 @@ function formatDates(dateString) {
 
 
 async function fetchAndDisplayMedicalDetails(candidateId) {
-    const token= localStorage.getItem('token')
     try {
         const response = await axios.get(`https://nemo.ivistaz.co/candidate/get-hospital-details/${candidateId}`, {
             headers: {
@@ -245,13 +244,16 @@ async function fetchAndDisplayMedicalDetails(candidateId) {
                 'Authorization': token
             }
         });
-
-        const hospitals = {}; // Map to store hospital details by ID
-        hospitalResponse.data.forEach(hospital => {
-            hospitals[hospital.id] = hospital.hospitalName; // Store hospital details by ID
+       const hospitalsmed = hospitalResponse.data.hospital
+        const hospitals = {}; // Map to store company details by ID
+        hospitalsmed.forEach(hospital => {
+            hospitals[hospital.id] = hospital.hospitalName; // Store company details by ID
         });
 
+
+
         const medicalDetails = response.data;
+        console.log(medicalDetails)
         const medicalTableBody = document.getElementById('hospitalTableBody');
         medicalTableBody.innerHTML = ''; // Clear existing rows
 
@@ -263,8 +265,7 @@ async function fetchAndDisplayMedicalDetails(candidateId) {
                 cell.textContent = value;
                 return cell;
             };
-
-            const hospitalName = hospitals[medical.hospitalId]; // Corrected to use hospital ID from the response
+            const hospitalName = hospitals[medical.hospitalName];
 
             // Add data to each cell
             row.appendChild(createCell(hospitalName));
@@ -306,7 +307,6 @@ async function fetchAndDisplayMedicalDetails(candidateId) {
         console.error(err);
     }
 }
-
 
 const editMedical = async (id, hospitalName, place, date, expiryDate, done_by, status, amount, uploadFile, event) => {
     event.preventDefault();
