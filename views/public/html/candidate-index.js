a170fed3b6c2a929cb5cffc550c40a76fdf75ce1
sibbe23 <sibbe23@gmail.com>
2024-06-28 18:56:02 +0530

    search

--- views/public/html/candidate-index.js
+++ views/public/html/candidate-index.js
@@ -36,7 +36,7 @@ document.getElementById("logout").addEventListener("click", function() {
   // Send request to update logged status to false
   const userId = localStorage.getItem('userId');
   if (userId) {
-    axios.put(`http://localhost:4000/user/${userId}/logout`)
+    axios.put(`https://nemo.ivistaz.co/user/${userId}/logout`)
       .then(response => {
         console.log('Logged out successfully');
       })
