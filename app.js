a7ae3dddb42db0e8b1467fd6d13be9b9b6dbbe26
sibbe23 <sibbe23@gmail.com>
2024-07-31 18:01:30 +0530

    test-new

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const otherRoutes = require("./routes/other")
 const { Op } = require('sequelize');
 const { QueryTypes } = require('sequelize');
 
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
