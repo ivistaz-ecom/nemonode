5ffa7850723b8276f7fc6a9d55858368ad8d9fc1
sibbe23 <sibbe23@gmail.com>
2024-08-06 11:34:18 +0530

    permission update 1

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 
-
 app.get('/home', (req, res) => {
     res.redirect("/aaaa.html");
 });
