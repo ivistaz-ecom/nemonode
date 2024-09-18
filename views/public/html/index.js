bca5547132abff6e55fc2fa249ad9aa401aef0be
sibbe23 <sibbe23@gmail.com>
2024-09-18 09:54:49 +0530

    UI reports dev 4

--- views/public/html/index.js
+++ views/public/html/index.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', async function () {
   const token = localStorage.getItem('token');
 
-
   const elements = {
     userName: document.getElementById('user_name'),
     userAvatar: document.getElementById('user-avatar'),
@@ -14,53 +13,41 @@ document.addEventListener('DOMContentLoaded', async function () {
     approvedCount: document.getElementById('approvedCount'),
     joinedCount: document.getElementById('joinedCount'),
     rejectedCount: document.getElementById('rejectedCount'),
-    proposedPercentageChange: document.getElementById('proposedPercentageChange'),
-    approvedPercentageChange: document.getElementById('approvedPercentageChange'),
-    joinedPercentageChange: document.getElementById('joinedPercentageChange'),
-    rejectedPercentageChange: document.getElementById('rejectedPercentageChange'),
-    proposedArrowIcon: document.getElementById('proposedArrowIcon'),
-    approvedArrowIcon: document.getElementById('approvedArrowIcon'),
-    joinedArrowIcon: document.getElementById('joinedArrowIcon'),
-    rejectedArrowIcon: document.getElementById('rejectedArrowIcon'),
     callCount: document.getElementById('callCount'),
-    percentageChange: document.getElementById('percentageChange'),
-    arrowIcon: document.getElementById('arrowIcon'),
     datetime: document.getElementById('datetime'),
     icon: document.querySelector('link[rel="icon"]')
   };
 
   showSkeletonLoading(elements);
 
-
   try {
     const [
       discussionCountsResponse,
       callCountResponse,
       statusCountResponse,
-      callCountFromModelResponse,
       candidateCountsResponse,
       rankCountsResponse
     ] = await Promise.all([
       axios.get('https://nsnemo.com/candidate/discussion-count', { headers: { "Authorization": token } }),
       axios.get('https://nsnemo.com/candidate/call-count', { headers: { "Authorization": token } }),
       axios.get('https://nsnemo.com/candidate/statuscount', { headers: { "Authorization": token } }),
-      axios.get('https://nsnemo.com/candidate/percentage', { headers: { "Authorization": token } }),
       axios.get('https://nsnemo.com/candidate/getCount', { headers: { "Authorization": token } }),
       axios.get('https://nsnemo.com/candidate/getGraph', { headers: { "Authorization": token } })
     ]);
+
     hideSkeletonLoading(elements);
 
     const discussionCountsData = discussionCountsResponse.data;
     const callCountData = callCountResponse.data;
     const statusCountData = statusCountResponse.data.counts[0];
-    const percentageData = callCountFromModelResponse.data;
     const rankCounts = rankCountsResponse.data.rankCounts;
     const candidateCounts = candidateCountsResponse.data;
 
     updateDiscussionCounts(discussionCountsData, elements);
-    updateCallCounts(callCountData, percentageData, elements);
-    updateStatusCounts(statusCountData, percentageData, elements);
+    updateCallCounts(callCountData, elements);
+    updateStatusCounts(statusCountData, elements);
     generateDoughnutChart(rankCounts, elements.rankCharts);
+    
     updateCandidatesCounts(candidateCounts, elements);
   } catch (error) {
     console.error('Error fetching data:', error);
@@ -71,15 +58,11 @@ document.addEventListener('DOMContentLoaded', async function () {
   updateDateTime(elements.datetime);
   setInterval(() => updateDateTime(elements.datetime), 1000);
   setIcon(elements.icon);
-  const hasUserManagement = decodedToken.userManagement;
-const currentuserId = decodedToken.userId
-console.log(currentuserId)
-console.log(hasUserManagement)
-if (hasUserManagement) {
-  document.getElementById('userManagementSection').style.display = 'block';
-  document.getElementById('userManagementSections').style.display = 'block';
-
-}
+  
+  if (decodedToken.userManagement) {
+    document.getElementById('userManagementSection').style.display = 'block';
+    document.getElementById('userManagementSections').style.display = 'block';
+  }
 
   document.getElementById("logout").addEventListener("click", handleLogout);
 
@@ -132,55 +115,22 @@ if (hasUserManagement) {
     });
   }
 
-  function updateCallCounts(callCountData, percentageData, elements) {
+  function updateCallCounts(callCountData, elements) {
     const callCountFromAPI = callCountData.call_count;
-    const callCountFromModel = percentageData.call_count;
-    const percentageChange = callCountFromAPI - callCountFromModel;
 
-    if (elements.callCount && elements.percentageChange && elements.arrowIcon) {
+    if (elements.callCount) {
       elements.callCount.textContent = callCountFromAPI;
       elements.callCount.className = 'text-dark';
-      elements.percentageChange.textContent = `${Math.abs(percentageChange)}`;
-
-      if (percentageChange > 0) {
-        elements.arrowIcon.className = 'bx bx-up-arrow-alt text-success';
-      } else if (percentageChange < 0) {
-        elements.arrowIcon.className = 'bx bx-down-arrow-alt text-danger';
-      } else {
-        elements.arrowIcon.className = 'bx bx-minus text-secondary';
-      }
     } else {
-      console.error('One or more elements are missing in the DOM.');
+      console.error('Call count element is missing in the DOM.');
     }
   }
 
-  function updateStatusCounts(statusCountData, percentageData, elements) {
-    const proposedPercentageChange = statusCountData.proposed_count - percentageData.proposed_count;
-    const approvedPercentageChange = statusCountData.approved_count - percentageData.approved_count;
-    const joinedPercentageChange = statusCountData.joined_count - percentageData.joined_count;
-    const rejectedPercentageChange = statusCountData.rejected_count - percentageData.rejected_count;
-
+  function updateStatusCounts(statusCountData, elements) {
     elements.proposedCount.textContent = statusCountData.proposed_count;
     elements.approvedCount.textContent = statusCountData.approved_count;
     elements.joinedCount.textContent = statusCountData.joined_count;
     elements.rejectedCount.textContent = statusCountData.rejected_count;
-
-    updateArrowIcon(elements.proposedArrowIcon, elements.proposedPercentageChange, proposedPercentageChange);
-    updateArrowIcon(elements.approvedArrowIcon, elements.approvedPercentageChange, approvedPercentageChange);
-    updateArrowIcon(elements.joinedArrowIcon, elements.joinedPercentageChange, joinedPercentageChange);
-    updateArrowIcon(elements.rejectedArrowIcon, elements.rejectedPercentageChange, rejectedPercentageChange);
-  }
-
-  function updateArrowIcon(arrowIconElement, percentageChangeElement, percentageChange) {
-    percentageChangeElement.textContent = Math.abs(percentageChange);
-
-    if (percentageChange > 0) {
-      arrowIconElement.className = 'bx bx-up-arrow-alt text-success';
-    } else if (percentageChange < 0) {
-      arrowIconElement.className = 'bx bx-down-arrow-alt text-danger';
-    } else {
-      arrowIconElement.className = 'bx bx-minus text-secondary';
-    }
   }
 
   function updateCandidatesCounts(candidateCounts, elements) {
@@ -223,7 +173,7 @@ if (hasUserManagement) {
           fontColor: '#333'
         },
         layout: {
-          padding: { left: 20, right: 20, top: 20, bottom: 20 }
+          padding: { left: 40, right: 40, top: 0, bottom: 40 }
         },
         animation: { duration: 0 }
       }
@@ -277,13 +227,10 @@ function handleLogout() {
   localStorage.clear();
 
   setTimeout(() => {
-    document.getElementById("logoutMessage").textContent = "Shutting down all sessions...";
-  }, 1000);
-
-  setTimeout(() => {
-    window.location.href = "loginpage.html";
-  }, 2000);
+    window.location.href = '../index.html';
+  }, 1500);
 }
+
 function showSkeletonLoading() {
   const skeletonElements = document.getElementsByClassName('skeleton-loading');
   for (let i = 0; i < skeletonElements.length; i++) {
@@ -297,24 +244,3 @@ function hideSkeletonLoading() {
     skeletonElements[i].style.display = 'none';
   }
 }
-
-function updateIconBasedOnTime() {
-  const icon = document.getElementById('icon');
-  
-  // Get the current hour
-  const currentHour = new Date().getHours();
-  
-  // Check if it's daytime (6 AM to 6 PM)
-  if (currentHour >= 6 && currentHour < 18) {
-      icon.classList.add('bx-sun'); // Add sun icon class
-      icon.classList.add('text-warning')
-      icon.classList.remove('bx-moon'); // Remove moon icon class if present
-  } else {
-      icon.classList.add('bx-moon'); // Add moon icon class
-      icon.classList.add('text-success')
-      icon.classList.remove('bx-sun'); // Remove sun icon class if present
-  }
-}
-
-// Call the function when the DOM is fully loaded
- updateIconBasedOnTime()
