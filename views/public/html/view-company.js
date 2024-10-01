be26d35f94e2f88e2e820f821fc08abd3f3feb55
prabanu9717 <88131244+prabanu9717@users.noreply.github.com>
2024-10-01 23:28:54 +0800

    change code structure

--- views/public/html/view-company.js
+++ views/public/html/view-company.js
@@ -1,6 +1,3 @@
-const token = localStorage.getItem('token');
-const decodedToken = decodeToken(token);
-
 window.onload = async function () {
 
     try {
@@ -39,7 +36,7 @@ function decodeToken(token) {
 
 async function displayCompanies(page = 1, limit = 10) {
     try {
-        const response = await axios.get(`https://nsnemo.com/company/view-company?page=${page}&limit=${limit}`, { headers: { "Authorization": token } });
+        const response = await axios.get(`${config.APIURL}company/view-company?page=${page}&limit=${limit}`, { headers: { "Authorization": token } });
         const companies = response.data.company;
         const companyList = document.getElementById("company-list");
         companyList.innerHTML = "";
@@ -122,7 +119,7 @@ async function deleteCompany(companyId, event) {
     event.preventDefault();
     let id = companyId;
     console.log(id);
-    const url = `https://nsnemo.com/company/delete-company/${id}`;
+    const url = `${config.APIURL}company/delete-company/${id}`;
     console.log(url);
     try {
         const response = await axios.delete(url, { headers: { "Authorization": token } });
@@ -158,55 +155,3 @@ const editCompany = async (companyId, companyname, contact_person, email, addres
         .join('&');
     window.location.href = `./edit-company.html?${queryString}`;
 };
-
-function updateDateTime() {
-    const dateTimeElement = document.getElementById('datetime');
-    const now = new Date();
-    const options = {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: true,
-        month: 'short',
-        day: 'numeric',
-        ordinal: 'numeric',
-    };
-    const dateTimeString = now.toLocaleString('en-US', options);
-    dateTimeElement.textContent = dateTimeString;
-}
-
-updateDateTime();
-setInterval(updateDateTime, 1000);
-
-document.getElementById("logout").addEventListener("click", function() {
-    // Display the modal with initial message
-    var myModal = new bootstrap.Modal(document.getElementById('logoutModal'));
-    myModal.show();
-    
-    // Send request to update logged status to false
-    const userId = localStorage.getItem('userId');
-    if (userId) {
-      axios.put(`https://nsnemo.com/user/${userId}/logout`)
-        .then(response => {
-          console.log('Logged out successfully');
-        })
-        .catch(error => {
-          console.error('Error logging out:', error);
-        });
-    } else {
-      console.error('User ID not found in localStorage');
-    }
-  
-    localStorage.clear();
-    
-    // Change the message and spinner after a delay
-    setTimeout(function() {
-        document.getElementById("logoutMessage").textContent = "Shutting down all sessions...";
-    }, 1000);
-  
-    // Redirect after another delay
-    setTimeout(function() {
-        window.location.href = "loginpage.html";
-    }, 2000);
-  });
-  
