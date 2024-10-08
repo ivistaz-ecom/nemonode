3467e71a166dabbb0d4fd30e8d334db5658efbf2
prabanu9717 <88131244+prabanu9717@users.noreply.github.com>
2024-10-08 19:08:00 +0800

    change code.

--- views/public/html/edit-port.js
+++ views/public/html/edit-port.js
@@ -1,20 +1,11 @@
-// Get the token from localStorage
-const token = localStorage.getItem('token');
-
-// Check if the token is not present
 if (!token) {
   // Redirect to the login page
-
   window.location.href = './loginpage.html';
 }
-
-
-let currentPage = 1; // Initialize current page
-
 async function displayPort(page = 1, limit = 10) {
     try {
         // Fetch ports from the server with pagination parameters
-        const portResponse = await axios.get(`https://nsnemo.com/others/view-port?page=${page}&limit=${limit}`, { headers: { "Authorization": token } });
+        const portResponse = await axios.get(`${config.APIURL}others/view-port?page=${page}&limit=${limit}`, { headers: { "Authorization": token } });
         console.log('Port Response:', portResponse);
 
         const portTable = document.getElementById("port-table");
@@ -106,40 +97,8 @@ async function displayPort(page = 1, limit = 10) {
 
 
 window.onload = async function () {
-
      displayPort();
-     const hasUserManagement = decodedToken.userManagement;
-     const vendorManagement = decodedToken.vendorManagement;
-     const staff = decodedToken.staff
-     console.log(vendorManagement);
-     if (hasUserManagement && decodedToken.userGroup !== 'vendor') {
-        document.getElementById('userManagementSection').style.display = 'block';
-        document.getElementById('userManagementSections').style.display = 'block';
-    }
-     if (vendorManagement) {
-        document.getElementById('vendorManagementSection').style.display = 'block';
-        document.getElementById('vendorManagementSections').style.display = 'block';
- 
-     }
-     if(staff)
-    {
-        document.getElementById('settingsContainer').style.display='none'
-        document.getElementById('settingsCard').style.display='block'
-    }
 };
-
-
-
-
-function decodeToken(token) {
-    // Implementation depends on your JWT library
-    // Here, we're using a simple base64 decode
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace('-', '+').replace('_', '/');
-    return JSON.parse(atob(base64));
-}
-const decodedToken = decodeToken(token);
-
 
 async function editPort(portId, portName, event) {
     event.preventDefault();
@@ -158,7 +117,7 @@ async function deletePort(portId, event) {
     event.preventDefault();
 
     const id = portId;
-    const url = `https://nsnemo.com/others/delete-port/${id}`;
+    const url = `${config.APIURL}others/delete-port/${id}`;
 
     try {
         const response = await axios.delete(url,{headers:{"Authorization":token}});
@@ -168,61 +127,3 @@ async function deletePort(portId, event) {
         console.error('Error during delete request:', error.message);
     }
 }
-
-document.getElementById("logout").addEventListener("click", function() {
-    // Display the modal with initial message
-    var myModal = new bootstrap.Modal(document.getElementById('logoutModal'));
-    myModal.show();
-    
-    // Send request to update logged status to false
-    const userId = localStorage.getItem('userId');
-    if (userId) {
-      axios.put(`https://nsnemo.com/user/${userId}/logout`)
-        .then(response => {
-          console.log('Logged out successfully');
-        })
-        .catch(error => {
-          console.error('Error logging out:', error);
-        });
-    } else {
-      console.error('User ID not found in localStorage');
-    }
-  
-    localStorage.clear();
-    
-    // Change the message and spinner after a delay
-    setTimeout(function() {
-        document.getElementById("logoutMessage").textContent = "Shutting down all sessions...";
-    }, 1000);
-  
-    // Redirect after another delay
-    setTimeout(function() {
-        window.location.href = "loginpage.html";
-    }, 2000);
-  });
-  
-
-
-
-function updateDateTime() {
-    const dateTimeElement = document.getElementById('datetime');
-    const now = new Date();
-
-    const options = {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: true,
-        month: 'short',
-        day: 'numeric',
-        ordinal: 'numeric',
-    };
-
-    const dateTimeString = now.toLocaleString('en-US', options);
-
-    dateTimeElement.textContent = dateTimeString;
-}
-
-// Update date and time initially and every second
-updateDateTime();
-setInterval(updateDateTime, 1000);
