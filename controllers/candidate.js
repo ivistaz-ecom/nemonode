a9ac090f04c6d3be2e1fa86766f194d53c892513
prabanu9717 <88131244+prabanu9717@users.noreply.github.com>
2025-01-22 20:39:23 +0800

    checking issue.

--- controllers/candidate.js
+++ controllers/candidate.js
@@ -4401,9 +4401,16 @@ const submitApplicationForm = async (req, res) => {
                     let issuedate = postData[`document_${doc.key}_issuedate`]||'';
                     console.log(issuedate, 'issuedateissuedateissuedateissuedateissuedate')
                     let issue_date = (issuedate!=="")?convertToDate(issuedate).replace(" 00:00:00", ""):'0000-00-00';
+                    if(issue_date==='Invalid date') {
+                        issue_date = '0000-00-00';
+                    }
                     let expirydate = postData[`document_${doc.key}_validuntill`]||'';
+                   
                     console.log(expirydate, 'expirydateexpirydateexpirydateexpirydate')
                     let expiry_date = (expirydate!=="")?convertToDate(expirydate).replace(" 00:00:00", ""):'0000-00-00';
+                    if(expiry_date==='Invalid date') {
+                        expiry_date = '0000-00-00';
+                    }
                     let issue_place = postData[`document_${doc.key}_issueplace`]||''; 
                     if(evaluation.length>0) {                       
                         let updatedFields = {
