688389a1e799c74aa6071301209435f038825d95
prabanu9717 <88131244+prabanu9717@users.noreply.github.com>
2025-03-27 20:05:02 +0800

    change cron running time

--- cronRunners/cronRun.js
+++ cronRunners/cronRun.js
@@ -1,33 +1,28 @@
 const cron = require("node-cron");
-const sequelize = require('../util/database')
-const axios = require('axios')
-require('dotenv').config()
-const Sib = require('sib-api-v3-sdk');
+const sequelize = require("../util/database");
+require("dotenv").config();
+const Sib = require("sib-api-v3-sdk");
 
 module.exports = {
   runCron: async function () {
-
-    //******************* Get and update[every 1-minit] the whatsapp status from twilio ******************/
-    cron.schedule("0 9 * * *", async function () {
-     // twilioWhatsapp.updateWhatsappStatus();
-     try {
-      const todaydate = new Date();
-      console.log(todaydate, 'todaydatetodaydate')
-      let date = todaydate.getMonth();
-      if(parseInt(date)<=9) {
-        date = `0${date}`;
-      }
-      let month = parseInt(todaydate.getMonth())+1;
-      if(parseInt(month)<=9) {
-        month = `0${month}`;
-      }
-      const emailToday = `${month}-${date}-${todaydate.getFullYear()}`;
-      const query = `SELECT candidateId, CONCAT(fname,' ',lname) AS name, c_mobi1, email1, c_rank, dob FROM Candidates WHERE dob LIKE '%-${month}-${date}%' AND dob!='1970-01-01' AND active_details=1`;
-      const existingCandidate = await sequelize.query(query, {
-          type: sequelize.QueryTypes.SELECT
-      });
-      if(existingCandidate.length>0) {
-        let htmlContent = `Dear Sir/Madam,<br/><br/>
+    cron.schedule("0 1 * * *", async function () {
+      try {
+        const todaydate = new Date();
+        let date = todaydate.getMonth();
+        if (parseInt(date) <= 9) {
+          date = `0${date}`;
+        }
+        let month = parseInt(todaydate.getMonth()) + 1;
+        if (parseInt(month) <= 9) {
+          month = `0${month}`;
+        }
+        const emailToday = `${month}-${date}-${todaydate.getFullYear()}`;
+        const query = `SELECT candidateId, CONCAT(fname,' ',lname) AS name, c_mobi1, email1, c_rank, dob FROM Candidates WHERE dob LIKE '%-${month}-${date}%' AND dob!='1970-01-01' AND active_details=1`;
+        const existingCandidate = await sequelize.query(query, {
+          type: sequelize.QueryTypes.SELECT,
+        });
+        if (existingCandidate.length > 0) {
+          let htmlContent = `Dear Sir/Madam,<br/><br/>
 
         I hope you’re doing well!<br/>
 
@@ -37,100 +32,44 @@ module.exports = {
        <table border="1" cellpadding="5" cellspacing="0">
                 <tr><td><b>Candidate ID</b></td><td><b>Name</b></td><td><b>Rank</b></td><td><b>Date Of Birth</b></td><td><b>Phone</b></td><td><b>Email</b></td></tr>
         `;
-        existingCandidate.map((item)=> {
-            htmlContent+= `<tr><td>${item.candidateId}</td><td>${item.name}</td><td>${item.c_rank}</td><td>${item.dob}</td><td>${item.c_mobi1}</td><td>${item.email1}</td></tr>`;
-        })
-         htmlContent+= `</table>`
+          existingCandidate.map((item) => {
+            htmlContent += `<tr><td>${item.candidateId}</td><td>${item.name}</td><td>${item.c_rank}</td><td>${item.dob}</td><td>${item.c_mobi1}</td><td>${item.email1}</td></tr>`;
+          });
+          htmlContent += `</table>`;
 
-        
-              const client = Sib.ApiClient.instance;
-              const apiKey = client.authentications['api-key'];
-              apiKey.apiKey = process.env.BREVO_API_KEY; // Ensure this environment variable is set
+          const client = Sib.ApiClient.instance;
+          const apiKey = client.authentications["api-key"];
+          apiKey.apiKey = process.env.BREVO_API_KEY; // Ensure this environment variable is set
 
-              const tranEmailApi = new Sib.TransactionalEmailsApi();
+          const tranEmailApi = new Sib.TransactionalEmailsApi();
 
-              const sender = {
-                  email: 'nautilusshipping@gmail.com',
-                  name: 'Nautilusshipping'
-              };
+          const sender = {
+            email: "nautilusshipping@gmail.com",
+            name: "Nautilusshipping",
+          };
 
-              const recipients = [
-                  { email: 'praba9717@gmail.com' },
-                  { email: 'crewing@nautilusshipping.com' },
-                  { email: 'ajay@nautilusshipping.com' },
-                  { email: 'Divakar@nautilusshipping.com' },
-              ];
-              console.log(recipients, 'recipientssss')
-           
-                  try {
-                      const response = await tranEmailApi.sendTransacEmail({
-                          sender,
-                          to: recipients, // Must be an array
-                          subject: `🎉 Birthday Notification – ${emailToday}`,
-                          htmlContent: htmlContent,
-                      });
+          const recipients = [
+            { email: "praba9717@gmail.com" },
+            { email: "crewing@nautilusshipping.com" },
+            { email: "ajay@nautilusshipping.com" },
+            { email: "Divakar@nautilusshipping.com" },
+          ];
+          console.log(recipients, "recipientssss");
 
-                      console.log('Email sent successfully:', response);
-                  } catch (error) {
-                      console.error('Error sending email:', error);
-                  }
-           
+          try {
+            const response = await tranEmailApi.sendTransacEmail({
+              sender,
+              to: recipients,
+              subject: `🎉 Birthday Notification – ${emailToday}`,
+              htmlContent: htmlContent,
+            });
 
-
-       /* 
-                // Get interviewer email from some source, e.g., a database or static list
-                const interviewerEmail = 'praba9717@gmail.com'
-        
-                // Send email to the interviewer
-                const client = Sib.ApiClient.instance;
-                const apiKey = client.authentications['api-key'];
-                apiKey.apiKey = process.env.BREVO_API_KEY;
-                const tranEmailApi = new Sib.TransactionalEmailsApi();
-                const sender = {
-                    email: 'mccivistasolutions@gmail.com',
-                    name: 'I-Vistaz'
-                };
-        
-                await tranEmailApi.sendTransacEmail({
-                    sender,
-                    to: 'praba9717@gmail.com',
-                    subject: 'Evaluation for Nemo Candidate',
-                    htmlContent: `
-                        <h2>Hello!</h2>
-                    `,
-                });
-                console.log('Email sent successfully');
- */
-        
-
-          /* console.log('key +>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>+>',process.env.BREVO_API_KEY)
-            const emailData = {
-                sender: { email: "mccivistasolutions@gmail.com" }, // Replace with your email
-                to: [
-                    { email: "praba9717@gmail.com" },
-                ],
-                subject: "Contracts Due Report",
-                htmlContent: "<p>Please find the attached Excel file.</p>",
-            };
-        
-            try {
-                console.log(process.env.BREVO_API_KEY)
-                const response = await axios.post('https://api.brevo.com/v3/smtp/email', emailData, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'api-key': process.env.BREVO_API_KEY // Ensure this is correct
-                        
-                    }
-                });
-                console.log(response, 'responseresponseresponseresponse')
-            
-            } catch (error) {
-                console.error('Error sending email:', error.response ? error.response.data : error.message);
-            } */
-
-      }
-     // console.log(existingCandidate, query, 'existingCandidate')
-      console.log("Im CRON to get whatsapp status from twilio and update in the whatsapp template table");
+            console.log("Email sent successfully:", response);
+          } catch (error) {
+            console.error("Error sending email:", error);
+          }
+        }
+        console.log("sending Birthday Mail");
       } catch (err) {
         console.log(err);
       }
