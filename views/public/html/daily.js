461f37bc5229c723dbca970f6b88c8c5a9c8b775
prabanu9717 <88131244+prabanu9717@users.noreply.github.com>
2025-04-29 00:08:58 +0800

    checking issue.

--- views/public/html/daily.js
+++ views/public/html/daily.js
@@ -827,7 +827,7 @@ async function loadAvailableCandidate() {
 
 function displayAvailableCandidate(availableList) {
   const convertedList = (availableList.length>0)?availableList.map(item => [item.c_rank, item.totalcandidate]):[];
-  document.getElementById("availableCandidate").style.height = `${parseInt(100*convertedList.length) + 100}px`;
+  document.getElementById("availableCandidate").style.height = `${parseInt(50*convertedList.length) + 100}px`;
   Highcharts.chart("availableCandidate", {
     chart: {
       animation: {
