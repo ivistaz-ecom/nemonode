b2886b4d996608622a821fd075f421afb1e93176
prabanu9717 <88131244+prabanu9717@users.noreply.github.com>
2025-06-12 22:03:45 +0800

    remove console.

--- middleware/auth.js
+++ middleware/auth.js
@@ -29,20 +29,14 @@ const authenticate = async (req, res, next) => {
 const authenticateEmloyee = async (req, res, next) => {
     try {
         const token = req.headers['authorization']; // Access the Authorization header
-         console.log(token)
         if (!token) {
             return res.status(401).json({ message: 'Authorization header missing', success: false });
         }
-
-        const userdata = jwt.verify(token, 'secretkey');
-        console.log(userdata)
-        // Fetch session information from the server based on userdata.userId
+        const userdata = jwt.verify(token, 'secretkey');      
         const user = await Employee.findByPk(userdata.employeeID);
-        // console.log(user)
         if (!user) {
             return res.status(401).json({ message: 'User not found', success: false });
         }
-
         req.user = user;
         next();
     } catch (err) {
